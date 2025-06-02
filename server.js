debe76721caf21ef5ef0daa5b6a062ad1a5c6afd
sumanth338 <sumanthashwin000@gmail.com>
2025-06-02 11:43:11 +0530

    this is my first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const server = express();
+
+server.get('/', (req, res)=>{
+    res.send('Welcome to the Ecommerce API')
+})
+
+server.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+});
