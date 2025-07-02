45d1a97fe1b22395fce80e03d2654f1a2201da2e
sumanth338 <sumanthashwin000@gmail.com>
2025-07-02 13:07:37 +0530

    JWT Authentication-1

--- src/features/user/user.controller.js
+++ src/features/user/user.controller.js
@@ -1,19 +1,21 @@
 import {userModel} from './user.model.js'
+import jwt from 'jsonwebtoken'
 
 export default class UserController {
     signup(req, res){
         const {name, email, password, type} = req.body
-       const user =  userModel.Signup(name, email, password, type)
+       const user =  userModel.signup(name, email, password, type)
         res.status(201).send(user)
     }
 
     signin(req, res){
-        const result = userModel.Signin(req.body.email, req.body.password)
+        const result = userModel.signin(req.body.email, req.body.password)
         if(!result){
             res.status(400).send("Invalid email or password")
         }
         else{
-            res.send("Login successful")
+            const token = jwt.sign({id: result.id,email:result.email},'sjfaskljfsa;jf',{expiresIn:'1h'})
+            res.status(200).send(token)
         }
     }
 }
